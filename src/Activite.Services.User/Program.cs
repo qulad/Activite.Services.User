ef25ee470dde0d5a1c5f1e74b3ff50d51c4b477c
Ahmet E. Kaya <ahmetertugrulkaya@gmail.com>
2024-11-10 10:46:31 +0300

    added Consul

---
+++ src/Activite.Services.User/Program.cs
@@ -0,0 +1,41 @@
+﻿using Convey;
+using Convey.Discovery.Consul;
+using Convey.HTTP;
+using Convey.Logging;
+using Convey.WebApi;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+
+#if DEBUG
+
+DotNetEnv.Env.Load();
+
+#endif
+
+var host = WebHost.CreateDefaultBuilder(args)
+    .ConfigureAppConfiguration((hostingContext, config) =>
+    {
+        config.AddEnvironmentVariables();
+    })
+    .ConfigureServices(services =>
+    {
+        services
+            .AddConvey()
+            .AddWebApi()
+            .AddHttpClient()
+            .AddConsul()
+            .Build();
+    })
+    .Configure(app =>
+    {
+        app
+            .UseConvey()
+            .UseEndpoints(endpoints => endpoints
+                .Get("/ping", ctx => ctx.Response.WriteAsync("pong")));
+    })
+    .UseLogging()
+    .Build();
+
+await host.RunAsync();
