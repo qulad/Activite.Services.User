e3ea766a9e4bba6d2dd2ca81b4b1b3117490e35d
Ahmet E. Kaya <ahmetertugrulkaya@gmail.com>
2024-11-23 22:34:38 +0300

    remove prop

--- src/Activite.Services.User/Mongo/Documents/TranslationDocuments.cs
+++ src/Activite.Services.User/Mongo/Documents/TranslationDocuments.cs
@@ -15,11 +15,7 @@ public class TranslationDocument : IIdentifiable<Guid>
 
     public decimal Region { get; set; }
 
-    public Guid CreatedBy { get; set; }
-
     public DateTimeOffset CreatedAt { get; set; }
-
-    public Guid? UpdatedBy { get; set; }
 
     public DateTimeOffset? UpdatedAt { get; set; }
 }
